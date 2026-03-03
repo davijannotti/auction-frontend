cdee5fd357c8056038c19416fc20b8637b0629cb
Vinícius de Souza Oliveira <viniso1903@gmail.com>
2026-03-03 17:42:40 -0300

    api/auctions.js file added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { theme } from './themes/MainTheme.js';
+import { theme } from "./themes/MainTheme.js";
 import Dashboard from "./Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
