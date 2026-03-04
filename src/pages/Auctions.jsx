f86a6d97c8954f331c1b1b3f8f47671ccb301268
Vinícius de Souza Oliveira <viniso1903@gmail.com>
2026-03-03 22:17:23 -0300

    use fetch to read the auction from the db

--- src/pages/Auctions.jsx
+++ src/pages/Auctions.jsx
@@ -1,67 +1,40 @@
 import AuctionCard from "../components/AuctionCard";
+import { fetchAuctions } from "../api/auctions";
+import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 
 export default function Auctions() {
-  const leiloesData = [
-    {
-      id: 1,
-      name: "PS5",
-      status: "Ativo",
-      category: "Games",
-      start: "01/03",
-      end: "10/03",
-    },
-    {
-      id: 2,
-      name: "Xbox",
-      status: "Ativo",
-      category: "Games",
-      start: "02/03",
-      end: "12/03",
-    },
-    {
-      id: 3,
-      name: "PC Master",
-      status: "Aguardando",
-      category: "Tech",
-      start: "15/03",
-      end: "20/03",
-    },
-    {
-      id: 3,
-      name: "PC Master",
-      status: "Encerrado",
-      category: "Tech",
-      start: "15/03",
-      end: "20/03",
-    },
-    {
-      id: 3,
-      name: "PC Master",
-      status: "Aguardando",
-      category: "Tech",
-      start: "15/03",
-      end: "20/03",
-    },
-    {
-      id: 3,
-      name: "PC Master",
-      status: "Cancelado",
-      category: "Tech",
-      start: "15/03",
-      end: "20/03",
-    },
-  ];
+  const [leiloesData, setLeiloesData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // Lógica para agrupar: { "Ativo": [...], "Aguardando": [...] }
-  const groupedLeiloes = leiloesData.reduce((acc, item) => {
-    if (!acc[item.status]) acc[item.status] = [];
-    acc[item.status].push(item);
-    return acc;
-  }, {});
+  useEffect(() => {
+    fetchAuctions()
+      .then((data) => setLeiloesData(data.results))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const groupedLeiloes = Array.isArray(leiloesData)
+    ? leiloesData.reduce((acc, item) => {
+        if (!acc[item.status]) acc[item.status] = [];
+        acc[item.status].push(item);
+        return acc;
+      }, {})
+    : {};
+
+  if (loading) {
+    return <p>Loading auctions...</p>;
+  }
 
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: 3, p: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        gap: 3,
+        p: 2,
+      }}
+    >
       {Object.keys(groupedLeiloes).map((status) => (
         <AuctionCard
           key={status}
